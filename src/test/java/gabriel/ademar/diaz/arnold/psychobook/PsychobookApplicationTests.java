e2cfab587b08563fe45b648d3e0b345236b6da1e
Gabri-2001 <arnoldstella2012@gmail.com>
2023-11-23 11:18:40 +0100

    init

---
+++ src/test/java/gabriel/ademar/diaz/arnold/psychobook/PsychobookApplicationTests.java
@@ -0,0 +1,13 @@
+package gabriel.ademar.diaz.arnold.psychobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
